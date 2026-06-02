2fa3f7b34bd4e360ff7ae715b0f00e5b7898f3c0
in7264 <in7264@gmail.com>
2026-06-02 16:11:50 +0300

    fix for render

--- TodoApp.API/Program.cs
+++ TodoApp.API/Program.cs
@@ -5,8 +5,22 @@ using TodoApp.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var rawConn = builder.Configuration.GetConnectionString("DefaultConnection")!;
+
+string connString;
+if (rawConn.StartsWith("postgresql://") || rawConn.StartsWith("postgres://"))
+{
+    var uri = new Uri(rawConn);
+    var userInfo = uri.UserInfo.Split(':');
+    connString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+else
+{
+    connString = rawConn;
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connString));
 
 builder.Services.AddScoped<TasksInterface, TaskService>();
 
